ad7d237cb0bcf65a87bb25cae22aa703ece512a1
Ador Shahriar <ador.shahriar@gmail.com>
2023-09-25 02:08:31 +0600

    donations page cards properly created

---
+++ src/components/SelectedDonationCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SelectedDonationCard = ({donation}) => {
+    const {picture, category, category_bg, card_bg, text_color, title, price} = donation
+    return (
+        <div style={{background: card_bg}} className='flex gap-6 items-center rouded-lg'>
+            <img className='w-[180px] h-[160px] md:w-[220px] md:h-[200px] object-cover' src={picture} alt="" />
+            <div>
+            <button style={{background: category_bg, color: text_color}} className='py-1 px-[10px] rounded-md font-medium text-sm'>{category}</button>
+            <h2 className='my-1 md:my-2 md:text-2xl text-lg font-semibold'>{title}</h2>
+            <h4 style={{color: text_color}} className='my-1 md:mb-5 font-semibold'>${price}</h4>
+            <button style={{background: text_color}} className='text-white font-semibold md:text-lg px-4 py-2 rounded-md'>View Details</button>
+            </div>
+        </div>
+    );
+};
+
+export default SelectedDonationCard;
--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SelectedDonationCard from '../components/SelectedDonationCard';
 
 const Donation = () => {
+    const [donations, setDonations] = useState([]);
+    const [displayAll, setDisplayAll] = useState(false);
+    useEffect(()=> {
+        const newDonations = JSON.parse(localStorage.getItem('donation-saved'));
+        if (!newDonations){
+            setDonations([])
+        }
+        else{
+            setDonations(newDonations);
+        }
+        
+    }, [])
+
+
     return (
-        <div>
-            this is donation
+        <div className="max-w-[1320px] mx-auto">
+            {
+                donations.length === 0 ? <div className='h-[70vh] flex justify-center items-center font-bold text-2xl'>No Donations Yet</div>:""
+            }
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mx-4">
+                {   
+                    !displayAll ? donations.slice(0,4).map(donation => <SelectedDonationCard key={donation.id} donation={donation}></SelectedDonationCard>):
+                    donations.map(donation => <SelectedDonationCard key={donation.id} donation={donation}></SelectedDonationCard>)
+                  
+                }
+            </div>
+            {
+                donations.length > 4 && !displayAll? <div className="text-center my-10">
+                    <button onClick={()=> setDisplayAll(true)} className="bg-[#009444] rounded-lg text-white px-7 py-3">See All</button>
+                </div>:<div></div>
+            }
         </div>
     );
 };
